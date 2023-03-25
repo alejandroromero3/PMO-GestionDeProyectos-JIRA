50d5572f7fad4159393b8f3856dc3514c74680e0
alejandroromero3 <alejanro4infor@gmail.com>
2023-03-25 12:39:31 +0100

    v2.0 Change Evolutive as Epic and Asignee

--- ScriptRunner/Scripts WorkFlows/Create Evolutive.groovy
+++ ScriptRunner/Scripts WorkFlows/Create Evolutive.groovy
@@ -3,12 +3,12 @@ Title: Creación Evolutive
 Description: Script para la creación de Evolutivos a través de una iniciativa,
              creación de issue de tipo Task enlazada a la issue de tipo Initiative
 Author: Alejandro Romero Juarros
-Version:1.0
+Version:2.0 Cambio de Evolutivo como épica en vez de task
 */
 
 //Definición de Variables
-//Evolutivo como Tarea/Task
-def issuetype = "10002"
+//Evolutivo como Épica
+def issuetype = "10000"
 def linkName = "Relates"
 def issueKey = issue.key
 
@@ -78,7 +78,7 @@ if(issue.fields.description instanceof Map){
     ]
 }
 
-//Cuerpo de la issue de tipo Task con todos los campos necesarios y sus respectivos valores
+//Cuerpo de la issue de tipo Épica con todos los campos necesarios y sus respectivos valores
 def body = [
     "fields": [
         "project": [
@@ -93,7 +93,8 @@ def body = [
             "id": asssigneeAccountId
         ],
         "customfield_10046": issue.fields.customfield_10046, // Scope
-        "duedate": issue.fields.duedate
+        "customfield_10011": issue.fields.summary, //Epic Name
+        "customfield_11152": issue.fields.customfield_11152 //Customer Date
     ]
 ]
 
@@ -116,8 +117,25 @@ body = [
     ]
 ]
 
-//Creación del issue link "Relates To" de la issue de tipo Iniciativa con la issue de tipo Task
+//Creación del issue link "Relates To" de la issue de tipo Iniciativa con la issue de tipo Épica
 post("/rest/api/3/issueLink")
     .header("Content-Type", "application/json")
     .body(body)
     .asObject(Map).body
+
+//Asignación de Responsable Project Manager (assignedPM) de la issue
+def assignedPM = issue.fields.customfield_10050?.accountId
+def assig
+if(assignedPM){
+    assig = assignedPM
+} else {
+    assig = null;
+}
+
+//Asignación de Responsable (assignee) de la issue
+def evol= newIssue.key
+if (assig) {
+def updateProject = put("/rest/api/3/issue/" + evol + "/assignee" )
+    .header("Content-Type", "application/json")
+    .body( accountId: assig).asString()
+}
