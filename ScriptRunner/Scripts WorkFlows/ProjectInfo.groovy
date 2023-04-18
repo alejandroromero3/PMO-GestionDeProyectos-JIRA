6cadb659afe0992e22289127c48743710c0f3841
alejandroromero3 <alejanro4infor@gmail.com>
2023-04-18 20:51:09 +0200

    datos para WF en PowerAutomate

--- ScriptRunner/Scripts WorkFlows/ProjectInfo.groovy
+++ ScriptRunner/Scripts WorkFlows/ProjectInfo.groovy
@@ -55,3 +55,27 @@ def Project = put("/rest/api/3/issue/" + issueKey)
     .body(body)
     .asString()
 }
+
+def areabody = resultParentIssue.body.fields.customfield_10046.value
+def confidentialbody = resultParentIssue.body.fields.customfield_10092.value
+def documentationbody = resultParentIssue.body.fields.customfield_11081.value 
+def projectLeaderbody = projectLeader.accountId
+def areamanagerbody= resultParentIssue.body.fields.customfield_11104?.accountId
+def subareareamanagerbody = resultParentIssue.body.fields.customfield_11202?.accountId
+def projectTeambody = projectTeam?.accountId
+
+def body2 = [
+        "name": "${projectName}", 
+        "key": "${projectKey}",
+        "area": "${areabody}", 
+        "confidential": "${confidentialbody}", 
+        "documentation": "${documentationbody}",
+        "project team": "${projectTeambody}",
+        "project leader": "${projectLeaderbody}",
+        "area manager": "${areamanagerbody}"
+]
+
+def PowerAutomate = post("https://prod-146.westeurope.logic.azure.com:443/workflows/c946dadcd15d478981d76250ea5ac580/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=4muPj7VdencMPGidOAs5pk8TWM9WfkuhTujPquL9Ouc")
+    .header("Content-Type", "application/json")
+    .body(body2)
+    .asString()
